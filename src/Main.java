7cf9df6130a21f3eaf5735fad8af15a8f223ee83
e.fedoseeva <tetstest@list.ru>
2021-06-25 01:21:52 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int cashNow = 100;
         int cashDeposit = 900;
-        int bonus = cashDeposit / 100;
-        int balance = cashNow + cashDeposit + bonus;
+        int bonus;
         if (cashDeposit < 1000) {
             bonus = 0;
         }
+            else {
+                bonus = cashDeposit / 100;
+        }
+        int balance = cashNow + cashDeposit + bonus;
         System.out.println("Итоговый баланс: " + balance + " Зачисленные бонусы: " + bonus);
     }
 }
